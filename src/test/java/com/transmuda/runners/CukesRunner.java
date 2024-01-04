c19f54a61cdcfab6dc5748134268fee116717774
ysaleh23 <ysaleh037@gmail.com>
2024-01-04 16:24:43 -0600

    Initial commit

---
+++ src/test/java/com/transmuda/runners/CukesRunner.java
@@ -0,0 +1,22 @@
+package com.transmuda.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features",
+        glue = "com/transmuda/step_definitions",
+        dryRun = false,
+        tags = "@login",
+        publish = true //generating a report with public link
+)
+public class CukesRunner {}
+
+
