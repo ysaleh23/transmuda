af8960b58b169cbd68eaf67bcd191e0465f7d758
simaayazz <simaayazz@gmail.com>
2024-01-09 13:23:40 -0600

    DashboardPage class created

---
+++ src/test/java/com/transmuda/pages/DashboardPage.java
@@ -0,0 +1,19 @@
+package com.transmuda.pages;
+
+import com.transmuda.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class DashboardPage {
+
+    public DashboardPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//a[@class='unclickable']//span[contains(text(), 'Fleet')]")
+    public WebElement fleetMenu;
+
+    @FindBy(xpath = "//span[.='Vehicles']")
+    public WebElement vehiclesLink;
+}
