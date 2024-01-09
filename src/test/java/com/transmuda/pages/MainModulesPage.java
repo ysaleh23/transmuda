1f42781a7edc5925f51411b84770d1c24c59aeb8
handesguven <handesukranguven@gmail.com>
2024-01-08 22:51:19 -0800

    Finished MainModules_StepDef and MainModulesPage class

--- src/test/java/com/transmuda/pages/MainModulesPage.java
+++ src/test/java/com/transmuda/pages/MainModulesPage.java
@@ -1,7 +1,11 @@
 package com.transmuda.pages;
 
 import com.transmuda.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class MainModulesPage {
 
@@ -9,5 +13,9 @@ public class MainModulesPage {
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
+
+
+    @FindBy(css = ".nav-multilevel.main-menu>li")
+    public List<WebElement> mainModulesName;
 
 }
--- src/test/java/com/transmuda/step_definitions/MainModules_StepDef.java
+++ src/test/java/com/transmuda/step_definitions/MainModules_StepDef.java
@@ -1,26 +1,44 @@
 package com.transmuda.step_definitions;
 
+import com.transmuda.pages.LoginPage;
+import com.transmuda.pages.MainModulesPage;
+import com.transmuda.utilities.BrowserUtils;
+import com.transmuda.utilities.ConfigurationReader;
+import com.transmuda.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class MainModules_StepDef {
+import java.time.Duration;
+import java.util.List;
+
+public class MainModules_StepDef{
+    LoginPage loginPage = new LoginPage();
+    MainModulesPage mainModulesPage = new MainModulesPage();
     @Given("the user logs in with valid credentials {string}, {string}")
-    public void the_user_logs_in_with_valid_credentials(String string, String string2) {
-
+    public void the_user_logs_in_with_valid_credentials(String username, String password) {
+        loginPage.login(username,password);
     }
     @When("the user navigates to the main page")
     public void the_user_navigates_to_the_main_page() {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.titleIs("Dashboard"));
+        BrowserUtils.verifyTitle("Dashboard");
 
     }
     @Then("the user should see the following modules:")
-    public void the_user_should_see_the_following_modules(io.cucumber.datatable.DataTable dataTable) {
-
+    public void the_user_should_see_the_following_modules(List<String> expectedModulesName) {
+       List<String> actualModulesName= BrowserUtils.getElementsText(mainModulesPage.mainModulesName);
+       Assert.assertEquals(expectedModulesName,actualModulesName);
     }
 
     @Given("the user can log in with valid credentials {string},{string}")
-    public void the_user_can_log_in_with_valid_credentials(String string, String string2) {
-
+    public void the_user_can_log_in_with_valid_credentials(String username, String password) {
+        loginPage.login(username,password);
     }
 
 
