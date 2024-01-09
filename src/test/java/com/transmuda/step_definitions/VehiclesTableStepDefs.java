957f21d70f706b9a79849fa1363f1f12616c5927
simaayazz <simaayazz@gmail.com>
2024-01-08 19:41:19 -0600

    VehiclesTableStepDefs class created

---
+++ src/test/java/com/transmuda/step_definitions/VehiclesTableStepDefs.java
@@ -0,0 +1,43 @@
+package com.transmuda.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class VehiclesTableStepDefs {
+
+    @Given("User hover over Fleet dropdown and selects Vehicles page option")
+    public void user_hover_over_fleet_dropdown_and_selects_vehicles_page_option() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user can see all the checkboxes as unchecked")
+    public void user_can_see_all_the_checkboxes_as_unchecked() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("The user checks the first checkbox")
+    public void the_user_checks_the_first_checkbox() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("all cars should be selected")
+    public void all_cars_should_be_selected() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user selects a specific car")
+    public void the_user_selects_a_specific_car() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("that car should be selected")
+    public void that_car_should_be_selected() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
