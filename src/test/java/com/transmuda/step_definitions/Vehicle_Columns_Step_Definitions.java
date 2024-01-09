08d9e1093b1d1e9320c869b76f2e02315c6a517e
jamalwoods08 <jamalwoods08@gmail.com>
2024-01-08 20:11:52 -0800

    Created implementation for Step Definitions

---
+++ src/test/java/com/transmuda/step_definitions/Vehicle_Columns_Step_Definitions.java
@@ -0,0 +1,68 @@
+package com.transmuda.step_definitions;
+
+import com.transmuda.pages.LoginPage;
+import com.transmuda.utilities.WebDriverFactory;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+
+public class Vehicle_Columns_Step_Definitions {
+
+
+
+
+
+
+
+    @Given(": I am logged in as Store manager")
+    public void i_am_logged_in_as_store_manager() {
+
+    }
+    @When(": Click on the fleet tab when on home page")
+    public void click_on_the_fleet_tab_when_on_home_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When(": Click on the vehicle model")
+    public void click_on_the_vehicle_model() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then(":Verify MODEL NAME, MAKE, CAN BE REQUESTED, CVVI,CO2 FEE \\(\\/MONTH), COST \\(DEPRECIATED), TOTAL COST \\(DEPRECIATED), CO2 EMISSIONS, FUEL TYPE, and VENDORS columns should be displayed")
+    public void verify_model_name_make_can_be_requested_cvvi_co2_fee_month_cost_depreciated_total_cost_depreciated_co2_emissions_fuel_type_and_vendors_columns_should_be_displayed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+    //--------------
+
+
+    @Given(": I am logged in as Sales manager")
+    public void i_am_logged_in_as_sales_manager() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When(": Click on the {string} when on home page")
+    public void click_on_the_when_on_home_page(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When(": Click on the {string}")
+    public void click_on_the(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then(":Verify {string} columns should be displayed")
+    public void verify_columns_should_be_displayed(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+
+
+
+
+}
