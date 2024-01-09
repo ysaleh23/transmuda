93547105764eeea34849b6294765377d8367cda3
handesguven <handesukranguven@gmail.com>
2024-01-09 10:15:26 -0800

    Updated cukesRunner

--- src/test/java/com/transmuda/runners/CukesRunner.java
+++ src/test/java/com/transmuda/runners/CukesRunner.java
@@ -9,7 +9,8 @@ import org.junit.runner.RunWith;
         plugin = {
                 "html:target/cucumber-reports.html",
                 "rerun:target/rerun.txt",
-                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "json:target/cucumber.json"
         },
         features = "src/test/resources/features",
         glue = "com/transmuda/step_definitions",
