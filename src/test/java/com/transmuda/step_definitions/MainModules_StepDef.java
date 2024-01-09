6ad675c607e9b3f70d0f06cb947c425e301c5179
handesguven <handesukranguven@gmail.com>
2024-01-08 19:58:47 -0800

    Created MainModule_stepDef class

---
+++ src/test/java/com/transmuda/step_definitions/MainModules_StepDef.java
@@ -0,0 +1,27 @@
+package com.transmuda.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MainModules_StepDef {
+    @Given("the user logs in with valid credentials {string}, {string}")
+    public void the_user_logs_in_with_valid_credentials(String string, String string2) {
+
+    }
+    @When("the user navigates to the main page")
+    public void the_user_navigates_to_the_main_page() {
+
+    }
+    @Then("the user should see the following modules:")
+    public void the_user_should_see_the_following_modules(io.cucumber.datatable.DataTable dataTable) {
+
+    }
+
+    @Given("the user can log in with valid credentials {string},{string}")
+    public void the_user_can_log_in_with_valid_credentials(String string, String string2) {
+
+    }
+
+
+}
