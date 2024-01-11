aa7296d5d761e31d09e1d255be806c5a3d55ea20
ysaleh23 <ysaleh037@gmail.com>
2024-01-11 10:05:28 -0600

    Removed Max's webelement locators.

--- src/test/java/com/transmuda/pages/CalenderEventPage.java
+++ src/test/java/com/transmuda/pages/CalenderEventPage.java
@@ -36,12 +36,6 @@ public class CalenderEventPage {
 //    @FindBy()
 //    public WebElement
 
-    //Maksym additional elements START
-    @FindBy(xpath = "//div[@id='mceu_24']/iframe")
-    public WebElement iFrame;
-
-    @FindBy(xpath = "//body[@id='tinymce']/p")
-    public WebElement textField;
 
 
 }
