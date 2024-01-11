364ccd4e854b4e3170fdcd08a869bb2dc8d9c71f
ysaleh23 <ysaleh037@gmail.com>
2024-01-10 19:30:20 -0600

    Changed the broken code, and then reran until the Test case PASSED!

--- src/test/java/com/transmuda/step_definitions/CalendarEvent_StepDefinitions.java
+++ src/test/java/com/transmuda/step_definitions/CalendarEvent_StepDefinitions.java
@@ -23,18 +23,20 @@ public class CalendarEvent_StepDefinitions {
 
     @Given("User hovers over  the Activities tab.")
     public void user_hovers_over_the_activities_tab() {
-        loginPage.login(ConfigurationReader.getProperty("driver_username"), ConfigurationReader.getProperty("driver_password"));
         BrowserUtils.sleep(2);
         actions.moveToElement(eventPage.activitiesTab).perform();
+
     }
     @When("User will click on the Calendar Events option from dropdown.")
     public void user_will_click_on_the_calendar_events_option_from_dropdown() {
-        BrowserUtils.sleep(2);
-        eventPage.calendarEventOption.click();
+        wait.until(ExpectedConditions.visibilityOf(eventPage.calendarEventOption)).click();
+
+
     }
     @When("User will click on the Create Calendar Event button")
     public void user_will_click_on_the_create_calendar_event_button() {
-        eventPage.createCalendarEventButton.click();
+        wait.until(ExpectedConditions.elementToBeClickable(eventPage.createCalendarEventButton)).click();
+
     }
     @When("User will click on the repeat button")
     public void user_will_click_on_the_repeat_button() {
@@ -44,8 +46,11 @@ public class CalendarEvent_StepDefinitions {
     @Then("Verify that users see the number {int} by default in the Repeat Every input option.")
     public void verify_that_users_see_the_number_by_default_in_the_repeat_every_input_option(int expectedDisplayedNumber) {
 
-        int actualDisplayedNumber = Integer.parseInt(eventPage.repeatEveryInputBoxWithNumber1.getText());
-        Assert.assertEquals(expectedDisplayedNumber, actualDisplayedNumber);
+        String expectedDisplayedNum = expectedDisplayedNumber + "";
+        String actualDisplayedNumber = eventPage.repeatEveryInputBoxWithNumber1.getAttribute("value");
+
+        Assert.assertEquals(expectedDisplayedNum,actualDisplayedNumber);
+
     }
 
     @When("User will clear the number {int} from the Repeat Every input option")
@@ -53,18 +58,12 @@ public class CalendarEvent_StepDefinitions {
         eventPage.repeatEveryInputBoxWithNumber1.clear();
     }
 
-    @Then("Verify that users see the error message {string} when the Calendar event Repeat Every field is cleared \\(delete the number {int}).")
-    public void verifyThatUsersSeeTheErrorMessageWhenTheCalendarEventRepeatEveryFieldIsClearedDeleteTheNumber(String expectedErrorMessage, int expectedInputNumber) {
+    @Then("Verify that users see the error message {string} when the Calendar event Repeat Every field is cleared.")
+    public void verify_that_users_see_the_error_message_when_the_calendar_event_repeat_every_field_is_cleared(String expectedDisplayedErrorMessage) {
 
-        String actualErrorMessage = eventPage.thisValueShouldNotBeBlankErrorMessage.getText();
-        int actualInputNumber = Integer.parseInt(eventPage.repeatEveryInputBoxWithNumber1.getText());
+        String actualDisplayedErrorMessage = eventPage.thisValueShouldNotBeBlankErrorMessage.getText();
 
-
-        Assert.assertEquals(expectedErrorMessage, actualErrorMessage);
-        if (expectedInputNumber != 1) {
-            Assert.assertEquals(expectedInputNumber, actualInputNumber);
-        }
-
+        Assert.assertEquals(expectedDisplayedErrorMessage,actualDisplayedErrorMessage);
 
     }
 }
