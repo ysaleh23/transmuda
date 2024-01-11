bc95917a5d389b1ebd8385b15220ba1deac5621c
Orkhan1492 <app.orkhan@gmail.com>
2024-01-11 11:21:29 -0800

    Updated my code to suit my scenario

--- src/test/java/com/transmuda/step_definitions/CarInfo_StepDefinitions.java
+++ src/test/java/com/transmuda/step_definitions/CarInfo_StepDefinitions.java
@@ -7,6 +7,7 @@ import com.transmuda.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.apache.commons.collections.list.TreeList;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -24,47 +25,31 @@ public class CarInfo_StepDefinitions {
 
     VehiclesPage vehiclesPage = new VehiclesPage();
     WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
-
     Actions actions = new Actions(Driver.getDriver());
     List<WebElement> actual_three_dots_webElements = new ArrayList<>();
+    List<WebElement> actual_three_dots_icons_webElements = new TreeList();
     JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
 
-    @Given("User hover over Fleet dropdown and selects Vehicles page option")
-    public void user_hover_over_fleet_dropdown_and_selects_vehicles_page_option() {
-//        BrowserUtils.sleep(3);
-//        wait.until(ExpectedConditions.visibilityOf(vehiclesPage.fleet_dropdown));
-//        vehiclesPage.fleet_dropdown.click();
-//        BrowserUtils.sleep(1);
-//        wait.until(ExpectedConditions.visibilityOf(vehiclesPage.vehicles_page));
-//        vehiclesPage.vehicles_page.click();
-//        BrowserUtils.sleep(2);
-    }
 
     @When("user hover the mouse over the three dots ...")
     public void user_hover_the_mouse_over_the_three_dots() {
 
         for (WebElement each_three_dots_WebElement : vehiclesPage.listOf_three_dots_webElements) {
-            actual_three_dots_webElements.add(each_three_dots_WebElement);
+            for (WebElement each_listOfThreeIcon : vehiclesPage.list_of_three_icons) {
+                actual_three_dots_webElements.add(each_three_dots_WebElement);
+                actual_three_dots_icons_webElements.add((each_listOfThreeIcon));
+            }
         }
-
-
-        actions.moveToElement(actual_three_dots_webElements.get(0)).clickAndHold().perform();
-        BrowserUtils.sleep(1);
-    }
-
-
-    @Then("users sees view, edit, delete options")
-    public void users_sees_view_edit_delete_options() {
-
-        BrowserUtils.waitForClickablility(vehiclesPage.view_icons,10);
-        BrowserUtils.waitForClickablility(vehiclesPage.edit_icons,10);
-        BrowserUtils.waitForClickablility(vehiclesPage.delete_icons,10);
-        Assert.assertTrue(vehiclesPage.view_icons.isDisplayed());
-        Assert.assertTrue(vehiclesPage.edit_icons.isDisplayed());
-        Assert.assertTrue(vehiclesPage.delete_icons.isDisplayed());
-        actions.release();
-
+        for (int i = 0; i < actual_three_dots_webElements.size(); i++) {
+            for (int j = 0; j < actual_three_dots_icons_webElements.size(); j++) {
+                actions.moveToElement(actual_three_dots_webElements.get(i)).click().perform();
+                wait.until(ExpectedConditions.visibilityOf(actual_three_dots_icons_webElements.get(i)));
+                Assert.assertTrue(actual_three_dots_icons_webElements.get(i).isDisplayed());
+            }
         }
 
     }
+}
+
+
 
