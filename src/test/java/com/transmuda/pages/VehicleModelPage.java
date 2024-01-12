fe73cd51461541f616880fb0284768c9798bf304
jamalwoods08 <jamalwoods08@gmail.com>
2024-01-11 18:05:50 -0800

    i have created my vehicle model  page

---
+++ src/test/java/com/transmuda/pages/VehicleModelPage.java
@@ -0,0 +1,32 @@
+package com.transmuda.pages;
+
+import com.transmuda.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class VehicleModelPage {
+
+    public VehicleModelPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//a[@href='/entity/Extend_Entity_VehiclesModel'] ")
+    public  WebElement vehicleModelLink;
+
+
+
+   @FindBy(xpath = "//a[@class='unclickable' and span[contains(text(),'Fleet')]]")
+        public WebElement fleetDropDownOptions;
+
+    @FindBy(xpath ="//thead[@class='grid-header']//th" )
+    public static List<WebElement> vehicleModelColumns;
+
+
+
+
+
+
+}
