aeff32ba015fcdf966faf0d289ef9c383532e122
Iskandarbek <97361054+Iskandarbek-GitHub@users.noreply.github.com>
2024-01-11 23:03:23 -0500

    I created all necessary web elements for my AC2

--- src/test/java/com/transmuda/runners/CukesRunner.java
+++ src/test/java/com/transmuda/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/transmuda/step_definitions",
         dryRun = false,
-        tags = "@roma",
+        tags = "@wip_filters",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
