5e15febfe7c6bcf732c3ec89e9009af137198ce0
Iskandarbek <97361054+Iskandarbek-GitHub@users.noreply.github.com>
2024-01-11 22:42:30 -0500

    @wip

--- src/test/java/com/transmuda/step_definitions/US_123_IZ_StepDefinition.java
+++ src/test/java/com/transmuda/step_definitions/US_123_IZ_StepDefinition.java
@@ -6,6 +6,7 @@ import com.transmuda.utilities.BrowserUtils;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.WebElement;
 
 
 public class US_123_IZ_StepDefinition {
@@ -25,8 +26,7 @@ public class US_123_IZ_StepDefinition {
     }
 
 
-    SubMenuOfFleetMenu subMenu = new SubMenuOfFleetMenu();
-    @Then("User should be able to see three columns TYPE, TOTAL PRICE, DATE")
+        @Then("User should be able to see three columns TYPE, TOTAL PRICE, DATE")
     public void user_should_be_able_to_see_three_columns_type_total_price_date() {
 
         Assert.assertEquals(costsPage.type.getText(), "TYPE");
@@ -34,5 +34,22 @@ public class US_123_IZ_StepDefinition {
         Assert.assertEquals(costsPage.date.getText(), "DATE");
 
     }
+
+    @When("User checks first checkbox User should see all checkboxes are checked")
+    public void user_checks_first_checkbox_user_should_see_all_checkboxes_are_checked() {
+        boolean isChecked = true;
+        for(WebElement eachCheckBox: vehiclesPage.checkboxes){ //iterating through each checkbox on the vehicle table for each car
+            if( !eachCheckBox.isSelected() ) {
+                isChecked = false; //if any of the cars are not selected, isChecked boolean's value will be assigned it to false
+                break;//it will terminate the for lop
+            }
+        }
+        Assert.assertTrue(isChecked);//expected isChecked value is true. This line will verify if all of the checkboxes are selected or not
+
+    }
+
+    /*
+
+     */
 
 }
--- src/test/resources/features/US_123_IZ_filters.feature
+++ src/test/resources/features/US_123_IZ_filters.feature
@@ -4,9 +4,13 @@ Feature: User should be able to manage filters on the Vehicle
 
   Background:
     Given the user logged in with username as "salesmanager101 " and password as "UserUser123"
-  @wip_filters
+
   Scenario: User should see 3 columns on the Vehicle Model page.
     Expected columns names: TYPE, TOTAL PRICE, DATE
 
     When User is on the home page user click on the main menu FLEET menu and VEHICLES COSTS on the submenu
     Then User should be able to see three columns TYPE, TOTAL PRICE, DATE
+
+  @wip_filters
+    Scenario: Users check the first checkbox to check all the Vehicle Costs
+      When User checks first checkbox User should see all checkboxes are checked
