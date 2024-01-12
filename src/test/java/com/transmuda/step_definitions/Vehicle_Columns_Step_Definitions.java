5d8563f0e10b3605b0b4a4f2842fe6a2e7d36b56
ysaleh23 <ysaleh037@gmail.com>
2024-01-11 21:45:47 -0600

    [Yousef]: Corrected the web elements

--- src/test/java/com/transmuda/step_definitions/Vehicle_Columns_Step_Definitions.java
+++ src/test/java/com/transmuda/step_definitions/Vehicle_Columns_Step_Definitions.java
@@ -1,67 +1,55 @@
 package com.transmuda.step_definitions;
 
-import com.transmuda.pages.LoginPage;
-import com.transmuda.utilities.WebDriverFactory;
-import io.cucumber.java.en.Given;
+import com.transmuda.pages.TransmudaDashBoardPage;
+import com.transmuda.pages.VehicleModelPage;
+import com.transmuda.utilities.BrowserUtils;
+import com.transmuda.utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.WebDriver;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Vehicle_Columns_Step_Definitions {
 
+    TransmudaDashBoardPage transmudaDashBoardPage = new TransmudaDashBoardPage();
+    VehicleModelPage modelPage = new VehicleModelPage();
+
+    Actions actions;
+    WebDriverWait wait;
 
 
-
-
-
-
-    @Given(": I am logged in as Store manager")
-    public void i_am_logged_in_as_store_manager() {
-
-    }
     @When(": Click on the fleet tab when on home page")
     public void click_on_the_fleet_tab_when_on_home_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        actions = new Actions(Driver.getDriver());
+        BrowserUtils.sleep(3);
+        actions.moveToElement(transmudaDashBoardPage.fleetDropDownOptions).perform();
+        BrowserUtils.sleep(3);
     }
+
     @When(": Click on the vehicle model")
     public void click_on_the_vehicle_model() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then(":Verify MODEL NAME, MAKE, CAN BE REQUESTED, CVVI,CO2 FEE \\(\\/MONTH), COST \\(DEPRECIATED), TOTAL COST \\(DEPRECIATED), CO2 EMISSIONS, FUEL TYPE, and VENDORS columns should be displayed")
-    public void verify_model_name_make_can_be_requested_cvvi_co2_fee_month_cost_depreciated_total_cost_depreciated_co2_emissions_fuel_type_and_vendors_columns_should_be_displayed() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        actions = new Actions(Driver.getDriver());
+        wait=new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOf(transmudaDashBoardPage.vehicleModelLink));
+        transmudaDashBoardPage.vehicleModelLink.click();
+
     }
 
+    @Then(": 	Verify that the column names")
+    public void verify_that_the_column_names(List<String> expectedColumnHeaderNames) {
+        BrowserUtils.sleep(3);
+        List<String> actualColumnHeaderNames = BrowserUtils.getElementsText(modelPage.vehicleModelColumns);
+       // BrowserUtils.sleep(3);
+        Assert.assertEquals(expectedColumnHeaderNames,actualColumnHeaderNames);
 
-    //--------------
-
-
-    @Given(": I am logged in as Sales manager")
-    public void i_am_logged_in_as_sales_manager() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
     }
-    @When(": Click on the {string} when on home page")
-    public void click_on_the_when_on_home_page(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @When(": Click on the {string}")
-    public void click_on_the(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then(":Verify {string} columns should be displayed")
-    public void verify_columns_should_be_displayed(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-
-
 
 
 
